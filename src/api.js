a6f8100f6f31881dfc83c8f50f00fe9b96e68525
Jesperwi <71511557+Jesperwi@users.noreply.github.com>
2020-12-22 09:06:08 +0100

    task4.4

--- src/api.js
+++ src/api.js
@@ -2,15 +2,15 @@ import { mockData } from '../src/mock-data';
 import axios from 'axios';
 import NProgress from 'nprogress';
 
-// const checkToken = async (accessToken) => {
-//   const result = await fetch(
-//     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-//   )
-//     .then((res) => res.json())
-//     .catch((error) => error.json());
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
 
-//   return result;
-// };
+  return result;
+};
 
 export const extractLocations = (events) => {
     var extractLocations = events.map((event) => event.location);
@@ -25,74 +25,88 @@ export const getEvents = async () => {
     NProgress.done();
     return mockData; 
 } 
+const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url = `https://le4j4w2z91.execute-api.us-east-1.amazonaws.com/dev/api/get-events/${token}`;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
+  }
 };
 
-// const token = await getAccessToken();
+const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
 
-//   if (token) {
-//     removeQuery();
-//     const url = `https://le4j4w2z91.execute-api.us-east-1.amazonaws.com/dev/api/get-events/${token}`;
-//     const result = await axios.get(url);
-//     if (result.data) {
-//       var locations = extractLocations(result.data.events);
-//       localStorage.setItem("lastEvents", JSON.stringify(result.data));
-//       localStorage.setItem("locations", JSON.stringify(locations));
-//     }
-//     NProgress.done();
-//     return result.data.events;
-//   }
-// };
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://le4j4w2z91.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
 
-// const getAccessToken = async () => {
-//   const accessToken = localStorage.getItem('access_token');
-//     const tokenCheck = accessToken && (await checkToken(accessToken));
+};
 
-//   if (!accessToken || tokenCheck.error) {
-//     await localStorage.removeItem("access_token");
-//     const searchParams = new URLSearchParams(window.location.search);
-//     const code = await searchParams.get("code");
-//     if (!code) {
-//       const results = await axios.get(
-//         "https://le4j4w2z91.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
-//       );
-//       const { authUrl } = results.data;
-//       return (window.location.href = authUrl);
-//     }
-//     return code && getToken(code);
-//   }
-//   return accessToken;
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
 
-// };
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    `https://le4j4w2z91.execute-api.us-east-1.amazonaws.com/dev/api/token/${encodeCode}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
 
-// const removeQuery = () => {
-//   if (window.history.pushState && window.location.pathname) {
-//     var newurl =
-//       window.location.protocol +
-//       "//" +
-//       window.location.host +
-//       window.location.pathname;
-//     window.history.pushState("", "", newurl);
-//   } else {
-//     newurl = window.location.protocol + "//" + window.location.host;
-//     window.history.pushState("", "", newurl);
-//   }
-// };
+  access_token && localStorage.setItem("access_token", access_token);
+
+  return access_token;
+};
 
 // const getToken = async (code) => {
+//   removeQuery();
 //   const encodeCode = encodeURIComponent(code);
 //   const { access_token } = await fetch(
-//     `https://le4j4w2z91.execute-api.us-east-1.amazonaws.com/dev/api/token/${encodeCode}`
+//   `https://f1k17pnw2a.execute-api.us-east-1.amazonaws.c
+//   om/dev/api/token/${encodeCode}`
 //   )
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .catch((error) => error);
-
-//   access_token && localStorage.setItem("access_token", access_token);
-
-//   return access_token;
+//   .then((res) => {
+//   return res.json();
+//   })
+//   .catch((error) => error);
+//   access_token &&
+// localStorage.setItem("access_token", access_token);
+// return access_token;
 // };
-///////////////////////////////
+
 
 // const checkToken = async (accessToken) => {
 //     const result = await fetch(
